9ab644add4334af376946bad262fb123c684e4f3
Foldi Bendeguz <bendeguz.foldi@gmail.com>
2016-11-23 11:05:34 +0100

    lamp telepites

---
+++ lamp.sh
@@ -0,0 +1,17 @@
+lxc launch ubuntu:16.04 x2
+lxc exec x2 -- apt -y update
+lxc exec x2 -- apt -y upgrade
+lxc exec x2 -- apt -y install mc
+lxc exec x2 -- apt -y install apache2
+lxc exec x2 -- apt -y install mysql-server mysql-client
+#service mysql status
+lxc exec x2 -- apt -y install php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-cgi libapache2-mod-php7.0 php-mbstring php7.0-mbstring php-gettext
+#php -v
+lxc exec x2 -- sh -c "echo $'<?php\nphpinfo();\n?>' > /var/www/html/test.php"
+lxc exec x2 -- service apache2 restart
+lxc exec x2 -- apt -y install phpmyadmin
+lxc exec x2 -- sh -c "echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf"
+lxc exec x2 -- service apache2 restart
+
+
+
